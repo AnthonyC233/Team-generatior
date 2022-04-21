bf833ab5e0b45499f577e3fd711075f56e619d39
AnthonyC233 <changanthony61@yahoo.com>
2022-04-20 23:22:08 -0400

    added code in index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const inquirer = require('inquirer');
+const Employee = require('./tests/Employee');
+const Manager = require('./tests/Manager');
+const Engineer = require('./tests/Engineer');
+const Intern = require('./tests/Intern');
+const fs = require('fs');
+
+team = [];
+    const managerQuestions = () => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the team managers name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the team managers id?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the team managers email?',
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'What is the team managers office number?',
+            },
+            {
+                type: 'list',
+                name: 'addMember',
+                message: 'What type of team member would you like to add?',
+                choices: ['Engineer', 'Intern', 'I dont want to add any more team members'],
+            }
+        ])}
